96f630e46d794760a742e5d1d6884840fcd08556
skibididopdopdopdopyesyes <skibididopdopdopdopyesyes0@gmail.com>
2026-06-02 12:36:31 +0300

    Create Config.java

---
+++ src/main/java/com/swill/killaura/Config.java
@@ -0,0 +1,17 @@
+package com.swill.killaura;
+
+public class Config {
+    public double range = 4.5;
+    public int minDelay = 50;
+    public int maxDelay = 150;
+    public boolean attackPlayers = true;
+    public boolean autoRotate = true;
+    public boolean smoothRotations = true;
+    public boolean autoBlock = true;
+    public boolean onlyWhenHoldingWeapon = true;
+    public boolean onlyWhenLooking = false;
+    public boolean teams = true;
+    public boolean invisibleCheck = true;
+    public boolean swingItem = true;
+    public String priority = "HEALTH";
+}
